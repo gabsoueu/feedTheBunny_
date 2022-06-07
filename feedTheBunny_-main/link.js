d47ee36829989f03b98e385dfba6853616b06e9d
gabsoueu <101360255+gabsoueu@users.noreply.github.com>
2022-06-07 17:00:30 -0300

    Add files via upload

---
+++ feedTheBunny_-main/link.js
@@ -0,0 +1,20 @@
+class Link{
+    constructor(bodyA,bodyB)
+    {
+        //bodyA = ultimo retangulo da corda
+        //bodyB = fruta
+        var lastlink = bodyA.body.bodies.length-2;
+        this.link = Constraint.create({
+            bodyA:bodyA.body.bodies[lastlink],
+            pointA:{x:0,y:0},
+            bodyB:bodyB,
+            pointB:{x:0,y:0},
+            length:20,
+            stiffness:0.02,
+        });
+        World.add(world,this.link);
+    }
+    remove () {
+        World.remove(world, this.link)
+    }
+}
